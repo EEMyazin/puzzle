0a5323b8f178ac5d5cef4a2fbc3c899b8d3a0e57
EEMyazin <e.e.myazin@gmail.com>
2020-01-29 17:49:02 +0500

    tests

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,17 @@
+import java.lang.reflect.Array;
+
 public class Solution {
     public static void main(String[] args) {
         ConundrumSolver sol = new RecursiveSolver();
-        int[] test = {1,2,3,4,0,5,6,7};
+        int[] test1 = {1,2,3,4,0,5,6,7};
         for(int i : sol.resolve(test){
             System.out.print(i + ' ');
         }
-        int[] test = {2,1,3,4,0,5,6,7};
+        int[] test2 = {2,1,3,4,0,5,6,7};
         for(int i : sol.resolve(test){
             System.out.print(i + ' ');
         }
-        int[] test = {0,1,2,3,4,5,6,7};
+        int[] test3 = {0,1,2,3,4,5,6,7};
         for(int i : sol.resolve(test){
             System.out.print(i + ' ');
         }
