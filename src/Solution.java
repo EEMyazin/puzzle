7a0e1fb19f023d213603ac104ed64ad78af29898
EEMyazin <e.e.myazin@gmail.com>
2020-01-29 17:59:20 +0500

    Correct issues

--- src/Solution.java
+++ src/Solution.java
@@ -4,15 +4,15 @@ public class Solution {
     public static void main(String[] args) {
         ConundrumSolver sol = new Solver();
         int[] test1 = {1,2,3,4,0,5,6,7};
-        for(int i : sol.resolve(test1){
+        for(int i : sol.resolve(test1)){
             System.out.print(i + ' ');
         }
         int[] test2 = {2,1,3,4,0,5,6,7};
-        for(int i : sol.resolve(test2){
+        for(int i : sol.resolve(test2)){
             System.out.print(i + ' ');
         }
         int[] test3 = {0,1,2,3,4,5,6,7};
-        for(int i : sol.resolve(test3){
+        for(int i : sol.resolve(test3)){
             System.out.print(i + ' ');
         }
     }
--- src/Solver.java
+++ src/Solver.java
@@ -1,19 +1,24 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Solver implements ConundrumSolver{
-    private List<int[]> solutions = new LinkedList<>();
+    private List<int[]> solutions;
+    private ArrayList<Integer> path;
     private int[][] connectivityMatrix= {{0,1,1,0,0,0,0,0},
-                                         {1,0,1,1,0,0,0,0},
-                                         {1,1,0,0,0,1,0,0},
-                                         {0,1,0,0,1,0,1,0},
-                                         {0,0,0,1,0,1,0,0},
-                                         {0,0,1,0,1,0,0,1},
-                                         {0,0,0,1,0,0,0,1},
-                                         {0,0,0,0,0,1,1,0}};
+                                        {1,0,1,1,0,0,0,0},
+                                        {1,1,0,0,0,1,0,0},
+                                        {0,1,0,0,1,0,1,0},
+                                        {0,0,0,1,0,1,0,0},
+                                        {0,0,1,0,1,0,0,1},
+                                        {0,0,0,1,0,0,0,1},
+                                        {0,0,0,0,0,1,1,0}};
     private int[] correctSolution = {1,2,3,4,0,5,6,7};
 
-    private int[] path;
+    public Solver(){
+        solutions = new LinkedList<>();
+        path = new ArrayList<>();
+    }
 
     @Override
     public int[] resolve(int[] initialState) {
