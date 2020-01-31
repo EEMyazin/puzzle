fe2e92d6779e6491e52a863916cadf2b19245aad
EEMyazin <e.e.myazin@gmail.com>
2020-01-31 12:31:44 +0500

    Add javadoc and more tests

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.paukov.combinatorics3.Generator;
 import org.w3c.dom.ls.LSOutput;
 
@@ -36,7 +38,15 @@ public class Solution {
         }
         int i = 0;
         for (Integer[] input : inputsArray){
-            System.out.println(Arrays.toString(input) + " -> " + Arrays.toString(sol.resolve(Arrays.stream(input).mapToInt(Integer::intValue).toArray())));
+            i++;
+            //System.out.println(Arrays.toString(input) + " -> " + Arrays.toString(sol.resolve(Arrays.stream(input).mapToInt(Integer::intValue).toArray())));
+            System.out.println("@Test\n" +
+                    "        public void resolve" + Integer.toString(i) + "() {\n" +
+                    "            int[] input = " + Arrays.toString(input).replace('[','{').replace(']','}') +";\n" +
+                    "            int[] actual = new SolverOnQueue().resolve(input);\n" +
+                    "            int[] expected = " + Arrays.toString(sol.resolve(Arrays.stream(input).mapToInt(Integer::intValue).toArray())).replace('[','{').replace(']','}') + ";\n" +
+                    "            Assert.assertArrayEquals(actual,expected);\n" +
+                    "        }\n");
         }
     }
 }
--- src/SolverOnQueue.java
+++ src/SolverOnQueue.java
@@ -23,23 +23,35 @@ public class SolverOnQueue implements ConundrumSolver {
     }
 
     /**
-     * Класс организующий поиск решения задачи
+     * Класс организующий поиск решения задачи. Решением будет путь, который пройдет пустая клетка.
      */
     class PathFinder {
+        /** Массив для хранения матрицы связности*/
         private final boolean[][] connectivityMatrix;
-        private final byte[] correctState;
+        /** Массив для хранения искомого состояния графа*/
+        private final State correctState;
 
+        /**
+         * Конструктор - создание нового поисковика
+         * @param connectivityMatrix матрица связности для обрабатываемого графа
+         * @param correctState искомое состояние графа
+         */
         public PathFinder(boolean[][] connectivityMatrix, byte[] correctState) {
             this.connectivityMatrix = connectivityMatrix;
-            this.correctState = correctState;
+            this.correctState = new State(correctState);
         }
 
+        /**
+         * Функция поиска искомого состояния графа, заданного матрицей связности из исходного состояния графа
+         * @param initialState исходное состояние графа
+         * @return путь пустой клетки для получения искомого состояния графа
+         */
         public int[] searchPathFrom(byte[] initialState){
             Queue queue = new Queue();
             queue.putState(new State(initialState));
             while (queue.hasStates()) {
                 State current = queue.getState();
-                if (current.isCorrectSolution(correctState)) {
+                if (current.equals(correctState)) {
                     return current.getPathAsIntArray();
                 }
                 queue.putStates(current.getNextStepStates(connectivityMatrix));
@@ -52,22 +64,34 @@ public class SolverOnQueue implements ConundrumSolver {
      * Класс реализующий очередь(FIFO) обработки состояний графа.
      */
     class Queue {
-        private LinkedList<State> queue;
-        private HashSet<Long> stateHistory;
+        /** Список для хранения состояний графа по которым необходимо произвести поиск */
+        private final LinkedList<State> queue;
+        /** Список хранящий предыдущие обработанные состояния графа*/
+        private final HashSet<Long> stateHistory;
 
+        /**
+         * Конструктор - создание новой очереди
+         */
         public Queue(){
-            queue = new LinkedList<State>();
-            stateHistory = new HashSet<Long>();
+            queue = new LinkedList<>();
+            stateHistory = new HashSet<>();
         }
 
+        /**
+         * Функция, возвращающая следующее состояние из очереди
+         * @return следующее состояние
+         */
         public State getState(){
             if (queue.size() == 0) {
                 return null;
             }
-            State result = queue.removeFirst();
-            return result;
+            return queue.removeFirst();
         }
 
+        /**
+         * Функция, помещающая состояние в очередь, при условии, что оно ранее не обрабатывалось
+         * @param newState новое состояние графа
+         */
         public void putState(State newState){
             if (!stateHistory.contains(newState.asLong())) {
                 queue.addLast(newState);
@@ -75,12 +99,20 @@ public class SolverOnQueue implements ConundrumSolver {
             }
         }
 
+        /**
+         * Функция, помещающая в очередь несколько состояний
+         * @param nextStates список новых состояний графа
+         */
         public void putStates(List<State> nextStates){
             for (State newState : nextStates){
                 putState(newState);
             }
         }
 
+        /**
+         * Функция проверяющая наличие состояний в очереди
+         * @return true если в очереди еще есть состояние; false если очередь пуста
+         */
         public boolean hasStates(){
             return (queue.size()>0);
         }
@@ -91,20 +123,17 @@ public class SolverOnQueue implements ConundrumSolver {
      * Класс для хранения состояния графа и пути его достижения.
      */
     class State{
+        /** Поле для хранения состояния графа*/
         private final byte[] state;         //Храним в байтах - меньше памяти потратится чем в int
+        /** Поле для хранения пути до текущего состояния графа*/
         private final Path path;
-
+        /** Вспомогательное поле для хранения положения нуля на графе */
         private int nullPosition; // Отслеживаем положение нуля, чтобы каждый раз не искать его в массиве
 
-        public byte[] getState() {
-            return state;
-        }
-
-        public int[] getPathAsIntArray() {
-            return path.AsIntArray();
-        }
-
-
+        /**
+         * Конструктор - создание нового состояния без пути
+         * @param state состояние графа
+         */
         public State(byte[] state){
             this.state = state;
             for (int i = 0; i < state.length; i++){
@@ -115,12 +144,24 @@ public class SolverOnQueue implements ConundrumSolver {
             this.path = new Path();
         }
 
+        /**
+         * Конструктор - создание нового состояния с определенными значениями
+         * @param state состояние графа
+         * @param path путь до этого состояния графа
+         * @param nullPosition индекс вершины в которой находится пустая ячейка
+         */
         private State(byte[] state, Path path, int nullPosition) {
             this.state = state;
             this.path = path;
             this.nullPosition = nullPosition;
         }
 
+        /**
+         * Создание нового состояния на основании текущего(для формирования используется индекс вершины из которой
+         * будет перемещена цифра)
+         * @param nextNullPosition индекс вершины
+         * @return полученное состояние графа
+         */
         private State getNextStepState(int nextNullPosition){
             byte[] newState = new byte[state.length];
             System.arraycopy(state, 0, newState, 0, state.length);
@@ -130,8 +171,29 @@ public class SolverOnQueue implements ConundrumSolver {
 
         }
 
+        /**
+         * Функция получения текущего расположения цифр в вершинах
+         * @return текущее расположение цифр
+         */
+        public byte[] getState() {
+            return state;
+        }
+
+        /**
+         * Возвращает текущее состояние графа
+         * @return текущее состояние графа как массив цифр в вершинах
+         */
+        public int[] getPathAsIntArray() {
+            return path.AsIntArray();
+        }
+
+        /**
+         * Возвращает состояния в которые может перейти граф из текущего
+         * @param connectivityMatrix матрица связности графа
+         * @return список состояний
+         */
         public LinkedList<State> getNextStepStates(boolean[][] connectivityMatrix){
-            LinkedList<State> nextStates = new LinkedList<State>();
+            LinkedList<State> nextStates = new LinkedList<>();
             for (int i = 0; i<state.length; i++){
                 if (connectivityMatrix[nullPosition][i] && state[i] != path.getLastStep()) {
                     nextStates.add(getNextStepState(i));
@@ -140,14 +202,21 @@ public class SolverOnQueue implements ConundrumSolver {
             return nextStates;
         }
 
-        public boolean isCorrectSolution(byte[] correctSolution){
-            return Arrays.equals(state,correctSolution);
-        }
+        /**
+         * Проверяет текущее состояние на соответствие другому
+         *
+         * @param other состояние для сравнения
+         * @return true расположение цифр в графах одинаково; false если различны
+         */
 
         public boolean equals(State other){
-            return Arrays.equals(state, other.getState());
+            return Arrays.equals(state,other.getState());
         }
 
+        /**
+         * Возвращает текущее расположение цифр в графе
+         * @return текущее расположение цифр в графе упакованное в Long
+         */
         public long asLong() {
             return ByteBuffer.wrap(state).getLong();
         }
@@ -158,24 +227,42 @@ public class SolverOnQueue implements ConundrumSolver {
          * Не вносятся изменения в текущие пути - для каждого возможного состояния графа создается свой путь его достижения.
          */
         class Path{
+            /**
+             * Поле для хранения "пути" пустой ячейки
+             */
             private final byte[] array;
             // Можно было использовать ArrayList<Byte>, но есть несколько минусов:
             // 1. При достижении размера массива он увеличивается в полтора раза, мне хотелось увеличивать вручную на меньшее
             // значение для экономии памяти
-            // 2. Не может хранить примитивы, Byte в памяти занимает больше чем byte. Мной ожидается большое количество
-            // хранимых "возможных путей"
+            // 2. Не может хранить примитивы, Byte в памяти занимает больше чем byte
+            /**
+             * Вспомогательное поле для хранения количества шагов в пути
+             */
             private final int countStep;
 
+            /**
+             * Конструктор для создания пустого пути
+             */
             public Path(){
                 array = new byte[10];
                 countStep = 0;
             }
 
+            /**
+             * Конструктор для создания пути
+             * @param array массив шагов в пути
+             * @param countStep количество шагов в пути
+             */
             public Path(byte[] array, int countStep){
                 this.array = array;
                 this.countStep = countStep;
             }
 
+            /**
+             * Функция возвращающая новый путь на основании текущего, в конец которого добавлен новый шаг
+             * @param nextStep в качестве шага передается цифра, которая будет передвинута в пустую ячейку
+             * @return путь полученный после шага
+             */
             public Path getPathWithNextStep(byte nextStep){
                 byte[] newArray;
                 if (array.length == countStep){
@@ -189,14 +276,22 @@ public class SolverOnQueue implements ConundrumSolver {
                 return new Path(newArray, countStep + 1);
             }
 
+            /**
+             * Возвращает текущий путь
+             * @return текущий путь как массив Int
+             */
             public int[] AsIntArray(){
                 int[] result = new int[countStep];
                 for (int i = 0; i < countStep; i++){
-                    result[i] = (int)array[i];
+                    result[i] = array[i];
                 }
                 return result;
             }
 
+            /**
+             * Возвращает последний шаг
+             * @return последняя цифра, поменявшаяся с пустой ячейкой
+             */
             public byte getLastStep(){
                 if (countStep == 0){
                     return 0;
