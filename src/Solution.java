60820e3468e1605da801c42659c0f80df8908d61
EEMyazin <e.e.myazin@gmail.com>
2020-01-31 12:35:19 +0500

    Cleaned code

--- src/Solution.java
+++ src/Solution.java
@@ -16,21 +16,6 @@ import static org.paukov.combinatorics3.Generator.*;
 public class Solution {
     public static void main(String[] args) {
         ConundrumSolver sol = new SolverOnQueue();
-        /*int[] test1 = {1, 2, 3, 4, 0, 7, 5, 6};
-        System.out.print('{');
-        for(int i : sol.resolve(test1)){
-            System.out.print(Integer.toString(i) + ", ");
-        }
-        System.out.print("};");*/
-        /*int[] test2 = {2,1,3,4,0,5,6,7};
-        for(int i : sol.resolve(test2)){
-            System.out.print(i + ' ');
-        }*/
-        /*int[] test3 = {7,2,3,4,1,5,6,0};
-        for(int i : sol.resolve(test3)){
-            System.out.print(Integer.toString(i) + ' ');
-        }*/
-        //System.out.println(Long.toString(ByteBuffer.wrap(new byte[]{1,2,3}).getLong()));
         List<List<Integer>> permutations = permutation(0, 1, 2, 3, 4, 5, 6, 7).simple().stream().collect(Collectors.toList());
         List<Integer[]> inputsArray = new LinkedList<Integer[]>();
         for (List<Integer> comb : permutations){
